7afed136b7dda3fdb9358d6ac6f71cbf65750af4
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2022-07-02 18:00:27 +0500

    update

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -2,7 +2,8 @@ import * as uzbek from './languages/uzbek.json';
 import * as english from './languages/english.json';
 import * as russian from './languages/russian.json';
 
-export const i18n = (language: string): any => {
+type language = 'uz' | 'en' | 'ru';
+export const i18n = (language: language): any => {
   switch (language) {
     case 'uz':
       return uzbek;
